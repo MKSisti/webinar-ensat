1a29f7c76ad446361ae509edae911166dc29bf6d
dvnkboi <aymandb@outlook.com>
2021-06-29 11:23:37 +0100

    v0.0.7 debounce and infinite scroll

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,25 @@
-function debounce(callback, interval) {
-  let debounceTimeoutId;
+function debounce(func, wait, immediate) {
+  var timeout;
 
-  return function(...args) {
-    clearTimeout(debounceTimeoutId);
-    debounceTimeoutId = setTimeout(() => callback.apply(this, args), interval);
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
   };
 }
 
-export default{
-    debounce
-}
+export {
+  debounce
+};
