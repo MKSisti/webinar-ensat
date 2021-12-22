796db9a2e495c5cb5b88e46c731b890fab121eb9
dvnkboi <aymandb@outlook.com>
2021-12-22 14:43:05 +0100

    v1.2.1 carousel loading fix

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -6,7 +6,9 @@
     @mouseenter="stopInterval"
     @mouseleave="startInterval"
   >
-    <div class="w-full h-full z-40 pointer-events-none flex justify-center items-start absolute space-x-3 px-4 py-4">
+    <div
+      class="w-full h-full z-40 pointer-events-none flex justify-center items-start absolute space-x-3 px-4 py-4"
+    >
       <span
         v-for="n in posts.length"
         :key="n"
@@ -17,16 +19,14 @@
     <!-- controls -->
     <div class="w-full h-full z-40 pointer-events-none flex justify-between items-center absolute">
       <div
-        :style="[dir == -1 ? transform: '']"
+        :style="[dir == -1 ? transform : '']"
         class="w-2/12 h-full flex justify-start items-center px-4 font-bold pointer-events-auto group cursor-pointer"
         @click="goToPrev()"
       >
         <span
           class="h-6 w-6 md:h-8 md:w-8 bg-gray-100 dark:bg-gray-900 shadow-lg flex items-center justify-center rounded-lg transform opacity-50 group-hover:translate-x-2 group-hover:scale-125 group-hover:opacity-100 transition duration-300"
-        ><i
-          class="ri-arrow-left-s-line mr-px text-sm mg:text-lg"
-          aria-hidden="true"
-        />
+        >
+          <i class="ri-arrow-left-s-line mr-px text-sm mg:text-lg" aria-hidden="true" />
         </span>
       </div>
       <div
@@ -36,28 +36,21 @@
       >
         <span
           class="h-6 w-6 md:h-8 md:w-8 bg-gray-100 dark:bg-gray-900 shadow-lg flex items-center justify-center rounded-lg transform opacity-50 group-hover:-translate-x-2 group-hover:scale-125 group-hover:opacity-100 transition duration-300"
-        ><i
-          class="ri-arrow-right-s-line ml-px text-sm mg:text-lg"
-          aria-hidden="true"
-        /></span>
+        >
+          <i class="ri-arrow-right-s-line ml-px text-sm mg:text-lg" aria-hidden="true" />
+        </span>
       </div>
     </div>
 
     <!-- slide container -->
-    <transition
-      :name=" dir > 0 ? 'fade-left' : 'fade-right'"
-      mode="in-out"
-      appear
-    >
+    <transition :name="dir > 0 ? 'fade-left' : 'fade-right'" mode="in-out" appear>
       <div
         :key="posts[current]?.pid"
         :style="currentImg"
         class="w-full h-full bg-cover absolute bg-center transform scale-105 transition duration-300 bg-no-repeat"
       />
     </transition>
-    <div
-      class="w-full h-full flex justify-center items-center"
-    >
+    <div class="w-full h-full flex justify-center items-center">
       <div
         class="h-12 md:h-16 left-0 bottom-0 absolute w-full group cursor-pointer focus:h-16 md:focus:h-20 transition-all duration-300"
         tabindex="0"
@@ -69,37 +62,30 @@
         />
 
         <!-- bottom user/article info -->
-        <transition
-          name="fade-y"
-          mode="out-in"
-          appear
-        >
+        <transition name="fade-y" mode="out-in" appear>
           <div
             v-if="!loading"
             :key="posts[current]?.pid + 'info'"
             class="absolute left-0 transition duration-300 bottom-0 w-full h-full px-2 xl:px-8 flex justify-start items-center z-50 opacity-80 group-focus:opacity-100"
           >
-            <span class="rounded-full bg-red-400 h-12 w-12 md:h-16 md:w-16 transform scale-75 group-focus:scale-100 transition duration-300 flex-none overflow-hidden">
-              <img
-                :src="user?.img"
-                alt=""
-              >
+            <span
+              class="rounded-full bg-red-400 h-12 w-12 md:h-16 md:w-16 transform scale-75 group-focus:scale-100 transition duration-300 flex-none overflow-hidden"
+            >
+              <img :src="user?.img" alt />
             </span>
-            <div class="flex flex-col justify-center items-start pl-4 w-full group-focus:pointer-events-auto pointer-events-none">
+            <div
+              class="flex flex-col justify-center items-start pl-4 w-full group-focus:pointer-events-auto pointer-events-none"
+            >
               <h1
                 :key="posts[current]?.title + 'title'"
                 class="font-bold opacity-100 cursor-pointer hover:underline text-xl md:text-2xl truncate w-8/12 transition duration-300 transform translate-y-3 -translate-x-3 group-focus:translate-y-0 group-focus:translate-x-0 z-50"
                 @click="goToPost(posts[current]?.pid)"
-              >
-                {{ posts[current]?.title }}
-              </h1>
+              >{{ posts[current]?.title }}</h1>
               <h2
                 :key="user?.uid + 'user'"
                 class="capitalize hover:underline cursor-pointer text-base md:text-lg transition duration-300 opacity-0 group-focus:opacity-100 z-20"
                 @click="goToUser(user?.uid)"
-              >
-                {{ user?.userName }}
-              </h2>
+              >{{ user?.userName }}</h2>
             </div>
           </div>
         </transition>
@@ -109,112 +95,112 @@
 </template>
 
 <script>
-  export default {
-    name: 'Carousel',
-    props: {
-      loop: Boolean,
-      posts: Array,
-      users: Map,
-      posters: Object,
-      loading: Boolean,
+export default {
+  name: 'Carousel',
+  props: {
+    loop: Boolean,
+    posts: Array,
+    users: Map,
+    posters: Object,
+    loading: Boolean,
+  },
+  data() {
+    return {
+      current: 0,
+      dir: 1,
+      transform: 0,
+    };
+  },
+  computed: {
+    currentImg() {
+      if (this.loading) return '';
+      if (!this.posters[this.posts[this.current]?.pid]) return "background-image: url(/img/icons/errorCover.jpg);";
+      return `background-image: url(${this.posters[this.posts[this.current]?.pid]})`;
     },
-    data() {
-      return {
-        current: 0,
-        dir: 1,
-        transform:0,
-      };
+    user() {
+      return this.users?.get(this.posts[this.current]?.owner);
     },
-    computed: {
-      currentImg() {
-        return `background-image: url(${this.posters[this.posts[this.current]?.pid]||"/img/icons/errorCover.jpg"})`;
-      },
-      user() {
-        return this.users?.get(this.posts[this.current]?.owner);
-      },
-    },
-    mounted() {
-      this.startInterval();
-      
-      let initialTouch, moveTouch;
-      this.$refs.slide.addEventListener('touchstart',(evt) => {
-        this.stopInterval();
-        initialTouch = evt.touches[0].clientX;
-        moveTouch = evt.touches[0].clientX;
-      },{passive: true});
+  },
+  mounted() {
+    this.startInterval();
 
-      this.$refs.slide.addEventListener('touchmove',(evt) => {
-        moveTouch = evt.touches[0].clientX;
-        this.dir = moveTouch - initialTouch < 0 ? 1 : -1;
-        this.transform = `transform: translateX(${(moveTouch - initialTouch)/4}px) scale(${(Math.abs(moveTouch - initialTouch) * 4 / window.screen.width) + 1})`;
-      },{passive: true});
-      
-      this.$refs.slide.addEventListener("touchend", ()=>{
-        this.transform = `transform: translateX(0px) scale(1)`;
-        if( Math.abs(moveTouch - initialTouch) > window.screen.width / 4){
-          moveTouch - initialTouch < 0 ? this.goToNext() : this.goToPrev();
-        }
-        this.startInterval();
-      },{passive: true});
-      // this.$refs.slide.addEventListener("touchcancel", this.startInterval, false);
+    let initialTouch, moveTouch;
+    this.$refs.slide.addEventListener('touchstart', (evt) => {
+      this.stopInterval();
+      initialTouch = evt.touches[0].clientX;
+      moveTouch = evt.touches[0].clientX;
+    }, { passive: true });
+
+    this.$refs.slide.addEventListener('touchmove', (evt) => {
+      moveTouch = evt.touches[0].clientX;
+      this.dir = moveTouch - initialTouch < 0 ? 1 : -1;
+      this.transform = `transform: translateX(${(moveTouch - initialTouch) / 4}px) scale(${(Math.abs(moveTouch - initialTouch) * 4 / window.screen.width) + 1})`;
+    }, { passive: true });
+
+    this.$refs.slide.addEventListener("touchend", () => {
+      this.transform = `transform: translateX(0px) scale(1)`;
+      if (Math.abs(moveTouch - initialTouch) > window.screen.width / 4) {
+        moveTouch - initialTouch < 0 ? this.goToNext() : this.goToPrev();
+      }
+      this.startInterval();
+    }, { passive: true });
+    // this.$refs.slide.addEventListener("touchcancel", this.startInterval, false);
+  },
+  methods: {
+    goToPost(pid) {
+      this.$router.push({
+        name: 'post',
+        params: { pid },
+      });
     },
-    methods: {
-      goToPost(pid) {
-        this.$router.push({
-          name: 'post',
-          params: { pid },
-        });
-      },
-      goToUser(uid) {
-        this.$router.push({
-          name: 'profile',
-          params: { uid },
-        });
-      },
-      goToNext() {
-        if(this.posts.length > 0){
-          this.dir = 1;
-          this.current = (this.current + 1) % this.posts.length;
-        }
-      },
-      goToPrev() {
-        if(this.posts.length > 0){
-          this.dir = -1;
-          this.current = (this.current - 1 + this.posts.length) % this.posts.length;
-        }
-      },
-      stopInterval() {
-        if (this.autoPlayInterval) clearInterval(this.autoPlayInterval);
-        this.autoPlayInterval = null;
-      },
-      startInterval() {
-        this.stopInterval();
-        this.autoPlayInterval = setInterval(this.goToNext, 6000);
-      },
-    }
-  };
+    goToUser(uid) {
+      this.$router.push({
+        name: 'profile',
+        params: { uid },
+      });
+    },
+    goToNext() {
+      if (this.posts.length > 0) {
+        this.dir = 1;
+        this.current = (this.current + 1) % this.posts.length;
+      }
+    },
+    goToPrev() {
+      if (this.posts.length > 0) {
+        this.dir = -1;
+        this.current = (this.current - 1 + this.posts.length) % this.posts.length;
+      }
+    },
+    stopInterval() {
+      if (this.autoPlayInterval) clearInterval(this.autoPlayInterval);
+      this.autoPlayInterval = null;
+    },
+    startInterval() {
+      this.stopInterval();
+      this.autoPlayInterval = setInterval(this.goToNext, 6000);
+    },
+  }
+};
 </script>
 
 <style>
+.fade-left-enter-from {
+  transform: translateX(20px) scale(105%);
+  opacity: 0;
+}
 
-  .fade-left-enter-from {
-    transform: translateX(20px) scale(105%);
-    opacity: 0;
-  }
+.fade-left-leave-to {
+  transform: translateX(-20px) scale(105%);
+  /* opacity: 0; */
+}
 
-  .fade-left-leave-to {
-    transform: translateX(-20px) scale(105%);
-    /* opacity: 0; */
-  }
+.fade-right-enter-from {
+  transform: translateX(-20px) scale(105%);
+  opacity: 0;
+}
 
-  .fade-right-enter-from {
-    transform: translateX(-20px) scale(105%);
-    opacity: 0;
-  }
-
-  .fade-right-leave-to {
-    transform: translateX(20px) scale(105%);
-    /* opacity: 0; */
-  }
-
+.fade-right-leave-to {
+  transform: translateX(20px) scale(105%);
+  /* opacity: 0; */
+}
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,33 +1,19 @@
 <template>
-  <transition
-    name="fade-y"
-    appear
-  >
+  <transition name="fade-y" appear>
     <div
       ref="home"
       class="overflow-auto flex flex-col justify-start items-center sm:space-y-10 xl:pt-10 transition duration-300"
     >
-      <carousel
-        :loading="carLoading"
-        :posters="carPosters"
-        :posts="carPosts"
-        :users="usersMap"
-      />
-      <div class="w-full xl:w-10/12 2xl:w-8/12 2xl:px-12 sm:px-8 xl:px-0 flex flex-col xl:flex-row justify-start items-center xl:justify-between xl:items-start xl:space-x-10 relative">
-        <loader
-          v-if="loading"
-          style="height:100px"
-          class="relative order-2 z-10 xl:order-1"
-        />
-        <div 
+      <carousel :loading="carLoading" :posters="carPosters" :posts="carPosts" :users="usersMap" />
+      <div
+        class="w-full xl:w-10/12 2xl:w-8/12 2xl:px-12 sm:px-8 xl:px-0 flex flex-col xl:flex-row justify-start items-center xl:justify-between xl:items-start xl:space-x-10 relative"
+      >
+        <loader v-if="loading" style="height:100px" class="relative order-2 z-10 xl:order-1" />
+        <div
           v-else
           class="flex flex-col order-2 z-10 xl:order-1 justify-center items-center xl:justify-start xl:w-7/12 w-full md:w-10/12 sm:space-y-10 relative"
         >
-          <transition-group
-            name="fade-y"
-            mode="in-out"
-            appear
-          >
+          <transition-group name="fade-y" mode="in-out" appear>
             <post-card
               v-for="p of posts"
               :key="p.pid"
@@ -37,15 +23,9 @@
               @click="goToPost(p.pid)"
             />
           </transition-group>
-          <loader
-            v-if="!loading && extraPosts"
-            class="pb-4 pt-4"
-          />
-          
-          <div
-            v-else-if="!loading && !extraPosts"
-            class="h-20 pt-4"
-          >
+          <loader v-if="!loading && extraPosts" class="pb-4 pt-4" />
+
+          <div v-else-if="!loading && !extraPosts" class="h-20 pt-4">
             <i
               class="ri-arrow-up-s-line transform transition duration-300 text-4xl"
               aria-hidden="true"
@@ -64,122 +44,121 @@
 </template>
 
 <script>
-  import Carousel from '../components/Carousel';
-  import PostCard from '../components/PostCard';
-  import Search from '../components/Search';
-  import Loader from '../components/Loader';
+import Carousel from '../components/Carousel';
+import PostCard from '../components/PostCard';
+import Search from '../components/Search';
+import Loader from '../components/Loader';
 
-  import { debounce } from '../utils';
-  // import {createPost} from '../js/firebaseActions'
-  import { makeUsersMap, getCWithRetry, getPosts } from '../js/dbActions';
+import { debounce } from '../utils';
+// import {createPost} from '../js/firebaseActions'
+import { makeUsersMap, getCWithRetry, getPosts } from '../js/dbActions';
 
-  export default {
-    name: 'App',
-    components: {
-      Carousel,
-      PostCard,
-      Search,
-      Loader,
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    PostCard,
+    Search,
+    Loader,
+  },
+  data() {
+    return {
+      loading: true,
+      postsToShow: 5,
+      posts: [],
+      usersMap: null,
+      keyword: '',
+      dropVal: 0,
+      extraPosts: true,
+      searching: false,
+      carPosts: [],
+      carPosters: {},
+      carLoading: true,
+      orderBy: [{ hosting_date: 1 }, { hosting_date: -1 }, { creation_date: 1 }, { creation_date: -1 }, { title: 1 }, { title: -1 }],
+    };
+  },
+  watch: {
+    $route: async function () {
+      if (this.$route.name == 'home') {
+        this.keyword = this.$route.query.keyword || '';
+        this.dropVal = this.$route.query.order || 0;
+        await this.doSearch();
+      }
     },
-    data() {
-      return {
-        loading: true,
-        postsToShow: 5,
-        posts: [],
-        usersMap: null,
-        keyword: '',
-        dropVal: 0,
-        extraPosts: true,
-        searching: false,
-        carPosts: [],
-        carPosters: {},
-        carLoading: true,
-        orderBy: [{ hosting_date: 1 }, { hosting_date: -1 }, { creation_date: 1 }, { creation_date: -1 }, { title: 1 }, { title: -1 }],
-      };
-    },
-    watch: {
-      $route: async function () {
-        if (this.$route.name == 'home') {
-          this.keyword = this.$route.query.keyword || '';
-          this.dropVal = this.$route.query.order || 0;
-          await this.doSearch();
+  },
+  async mounted() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      this.$nextTick(async () => {
+        let op = { approved: true };
+        if (this.$route.query?.keyword) {
+          this.keyword = this.$route.query.keyword;
+          op['title'] = this.keyword;
         }
-      },
-    },
-    async mounted() {
-      this.init();
-    },
-    methods: {
-      async init(){
-        this.$nextTick(async () => {
-          let op = { approved: true };
-          if (this.$route.query?.keyword) {
-            this.keyword = this.$route.query.keyword;
-            op['title'] = this.keyword;
-          }
-          if (this.$route.query?.order) {
-            this.dropVal = this.$route.query.order;
-          }
-          this.posts = await getPosts(op, this.orderBy[this.dropVal], this.postsToShow, null);
-          this.posts.length > 0 ? (this.usersMap = await makeUsersMap(this.posts, this.usersMap)) : null;
-
-          this.carPosts = await getPosts({ approved: true }, { hosting_date: 1 }, 3, null);
-
-          this.carPosts.forEach(async (p) => {
-            const cover = await getCWithRetry(p.pid);
-            this.carPosters[p.pid] = cover;
-          });
-          this.carLoading = false;
-
-          this.loading = false;
-          let debScrollBottom = debounce(async () => {
-            if (!this.loading)
-              if (this.$refs.home?.scrollHeight && this.$refs.home?.scrollHeight - this.$refs.home?.scrollTop <= this.$refs.home?.clientHeight) {
-                this.extraPosts = true;
-                let extra = await getPosts(this.keyword ? { approved: true, title: this.keyword } : { approved: true }, this.orderBy[this.dropVal], this.postsToShow, this.posts[this.posts.length - 1]);
-                if (extra.length > 0) {
-                  this.posts.push(...extra);
-                  this.usersMap = await makeUsersMap(extra, this.usersMap);
-                } else {
-                  setTimeout(() => (this.extraPosts = false), 1000);
-                }
-              }
-          }, 200);
-
-          this.$refs.home.addEventListener('scroll', debScrollBottom, false);
-        });
-      },
-      goToPost(pid) {
-        this.$router.push({
-          name: 'post',
-          params: {
-            pid: pid,
-          },
-        });
-      },
-      async doSearch() {
-        this.loading = true;
-        if (this.keyword != '') {
-          this.searching = true;
-          this.posts = await getPosts({ approved: true, title: this.keyword }, this.orderBy[this.dropVal], this.postsToShow);
-        } else {
-          this.searching = false;
-          this.posts = await getPosts({ approved: true }, this.orderBy[this.dropVal], this.postsToShow, null);
+        if (this.$route.query?.order) {
+          this.dropVal = this.$route.query.order;
         }
-        this.usersMap = await makeUsersMap(this.posts, this.usersMap);
+        this.posts = await getPosts(op, this.orderBy[this.dropVal], this.postsToShow, null);
+        this.posts.length > 0 ? (this.usersMap = await makeUsersMap(this.posts, this.usersMap)) : null;
+
+        this.carPosts = await getPosts({ approved: true }, { hosting_date: 1 }, 3, null);
+        for (let p of this.carPosts) {
+          const cover = await getCWithRetry(p.pid);
+          this.carPosters[p.pid] = cover;
+        };
+        this.carLoading = false;
+
         this.loading = false;
-      },
-      async search(searchVal, dropDownVal) {
-        this.$router.push({
-          name: 'home',
-          query: {
-            keyword: searchVal,
-            order: dropDownVal,
-          },
-        });
-      },
+        let debScrollBottom = debounce(async () => {
+          if (!this.loading)
+            if (this.$refs.home?.scrollHeight && this.$refs.home?.scrollHeight - this.$refs.home?.scrollTop <= this.$refs.home?.clientHeight) {
+              this.extraPosts = true;
+              let extra = await getPosts(this.keyword ? { approved: true, title: this.keyword } : { approved: true }, this.orderBy[this.dropVal], this.postsToShow, this.posts[this.posts.length - 1]);
+              if (extra.length > 0) {
+                this.posts.push(...extra);
+                this.usersMap = await makeUsersMap(extra, this.usersMap);
+              } else {
+                setTimeout(() => (this.extraPosts = false), 1000);
+              }
+            }
+        }, 200);
+
+        this.$refs.home.addEventListener('scroll', debScrollBottom, false);
+      });
     },
-  };
+    goToPost(pid) {
+      this.$router.push({
+        name: 'post',
+        params: {
+          pid: pid,
+        },
+      });
+    },
+    async doSearch() {
+      this.loading = true;
+      if (this.keyword != '') {
+        this.searching = true;
+        this.posts = await getPosts({ approved: true, title: this.keyword }, this.orderBy[this.dropVal], this.postsToShow);
+      } else {
+        this.searching = false;
+        this.posts = await getPosts({ approved: true }, this.orderBy[this.dropVal], this.postsToShow, null);
+      }
+      this.usersMap = await makeUsersMap(this.posts, this.usersMap);
+      this.loading = false;
+    },
+    async search(searchVal, dropDownVal) {
+      this.$router.push({
+        name: 'home',
+        query: {
+          keyword: searchVal,
+          order: dropDownVal,
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
